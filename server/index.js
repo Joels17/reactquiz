65c19d144e4c73fb38500a06d42a01b4d7e8d4fa
Joels17 <joel.semeniuk@gmail.com>
2021-11-03 01:02:41 -0700

    Added index serve

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// const buildPath = path.join(__dirname, '..', 'build');
-// app.use(express.static(buildPath));
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
 
 
 // This is the nodejs function here
@@ -56,6 +56,9 @@ app.get("/api/temps", async (req, res) => {
   res.json(retValue);
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
